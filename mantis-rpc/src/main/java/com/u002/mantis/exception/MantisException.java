f8d07484a13baa14d86aa33d186257a67a6bb95b
cuidd <cuidongdong@enn.cn>
2024-06-06 18:16:16 +0800

    全局运行时异常定义。

---
+++ mantis-rpc/src/main/java/com/u002/mantis/exception/MantisException.java
@@ -0,0 +1,14 @@
+package com.u002.mantis.exception;
+
+/**
+ * 运行时异常定义
+ */
+public class MantisException extends RuntimeException {
+    /**
+     * 构造函数
+     * @param msg 异常信息
+     */
+    public MantisException(String msg) {
+        super(msg);
+    }
+}
