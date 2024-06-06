7ebb58c624a1a1840f684204ec2eaac97d8396e2
cuidd <cuidongdong@enn.cn>
2024-06-06 18:42:17 +0800

    远程调用客户端封装。

---
+++ mantis-rpc/src/main/java/com/u002/mantis/client/RpcClient.java
@@ -0,0 +1,45 @@
+package com.u002.mantis.client;
+
+
+import com.u002.mantis.Client;
+import com.u002.mantis.proxy.IAsyncObjectProxy;
+import com.u002.mantis.proxy.IProxy;
+import com.u002.mantis.proxy.InternalProxy;
+import com.u002.mantis.proxy.ObjectProxyHandler;
+import com.u002.mantis.remote.ConnectManager;
+import com.u002.mantis.remote.RemoteCall;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class RpcClient implements Client {
+
+    //代理类本地做了个缓存
+    private final Map<Class, Object> proxyInstances = new ConcurrentHashMap<>();
+
+    private final long timeout = 2000;
+
+    private final IProxy proxy = new InternalProxy();
+
+
+    @Override
+    public <T> T create(Class<T> interfaceClass) {
+        if (proxyInstances.containsKey(interfaceClass)) {
+            return (T) proxyInstances.get(interfaceClass);
+        } else {
+            Object object = proxy.createInstance( interfaceClass, new ObjectProxyHandler<>(new RemoteCall<>(timeout)),Thread.currentThread().getContextClassLoader());
+            proxyInstances.put(interfaceClass, object);
+            return (T) object;
+        }
+    }
+
+    @Override
+    public <T> IAsyncObjectProxy createAsync(Class<T> interfaceClass) {
+        return new RemoteCall(interfaceClass, timeout);
+    }
+
+    public void stop() {
+        ConnectManager.getInstance().stop();
+    }
+
+}
