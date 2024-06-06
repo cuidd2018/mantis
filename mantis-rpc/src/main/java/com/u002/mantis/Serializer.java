d46f1d2734f793e40ebc6c1e8c0497b768faa254
cuidd <cuidongdong@enn.cn>
2024-06-06 18:29:03 +0800

    序列化接口。

---
+++ mantis-rpc/src/main/java/com/u002/mantis/Serializer.java
@@ -0,0 +1,29 @@
+package com.u002.mantis;
+
+
+import java.io.IOException;
+
+
+/**
+ * @author			koko
+ * @date 			2017-08-08 14:25:43
+ * @description 	序列化扩展点
+ */
+public interface Serializer {
+
+	/**
+	 * 序列化
+	 * @param data 对象
+	 * @throws IOException io异常
+	 */
+	<T> byte[] serialize(T data)throws IOException;
+
+	/**
+	 *
+	 * @param type clazz
+	 * @return 反序列haul的对象
+	 * @throws IOException io异常
+	 */
+    <T> T deserialize(byte[] bytes, Class<T> type)throws IOException;
+	
+}
