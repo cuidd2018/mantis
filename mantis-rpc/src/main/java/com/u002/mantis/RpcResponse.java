1e80f7543182b01123237b42ca6655618cfd1b84
cuidd <cuidongdong@enn.cn>
2024-06-06 18:35:54 +0800

    响应消息体封装。

---
+++ mantis-rpc/src/main/java/com/u002/mantis/RpcResponse.java
@@ -0,0 +1,38 @@
+package com.u002.mantis;
+
+import java.io.Serializable;
+
+public class RpcResponse implements Serializable {
+
+    private String requestId;
+    private String error;
+    private Object result;
+
+    public String getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(String requestId) {
+        this.requestId = requestId;
+    }
+
+    public boolean isError() {
+        return error != null;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public Object getResult() {
+        return result;
+    }
+
+    public void setResult(Object result) {
+        this.result = result;
+    }
+}
