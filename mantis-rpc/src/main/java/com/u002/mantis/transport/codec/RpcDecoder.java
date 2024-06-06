b2aabf883983aa2d65016969ee62e8deef1cdd69
cuidd <cuidongdong@enn.cn>
2024-06-06 18:39:16 +0800

    编解码器实现。

---
+++ mantis-rpc/src/main/java/com/u002/mantis/transport/codec/RpcDecoder.java
@@ -0,0 +1,39 @@
+package com.u002.mantis.transport.codec;
+
+
+import com.u002.mantis.Serializer;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+import java.util.List;
+
+public class RpcDecoder extends ByteToMessageDecoder {
+
+    private final Class<?> genericClass;
+
+    private final Serializer serializer;
+
+    public RpcDecoder(Class<?> genericClass, Serializer serializer) {
+        this.genericClass = genericClass;
+        this.serializer = serializer;
+    }
+
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+        if (in.readableBytes() < 4) {
+            return;
+        }
+        in.markReaderIndex();
+        int dataLength = in.readInt();
+        if (in.readableBytes() < dataLength) {
+            in.resetReaderIndex();
+            return;
+        }
+        byte[] data = new byte[dataLength];
+        in.readBytes(data);
+
+        Object obj = serializer.deserialize(data, genericClass);
+        out.add(obj);
+    }
+}
---
+++ mantis-rpc/src/main/java/com/u002/mantis/transport/codec/RpcEncoder.java
@@ -0,0 +1,26 @@
+package com.u002.mantis.transport.codec;
+
+import com.u002.mantis.Serializer;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+
+public class RpcEncoder extends MessageToByteEncoder {
+
+    private Class<?> genericClass;
+
+    private final Serializer serializer;
+
+    public RpcEncoder(Class<?> genericClass, Serializer serializer) {
+        this.genericClass = genericClass;
+        this.serializer = serializer;
+    }
+
+    protected void encode(ChannelHandlerContext ctx, Object in, ByteBuf out) throws Exception {
+        if (genericClass.isInstance(in)) {
+            byte[] data = serializer.serialize(in);
+            out.writeInt(data.length);
+            out.writeBytes(data);
+        }
+    }
+}
