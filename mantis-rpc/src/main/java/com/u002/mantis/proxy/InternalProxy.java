80acd173fe91c9bc317281b25d4de6bc8396cd61
cuidd <cuidongdong@enn.cn>
2024-06-06 18:20:17 +0800

    默认代理实现类。

---
+++ mantis-rpc/src/main/java/com/u002/mantis/proxy/InternalProxy.java
@@ -0,0 +1,26 @@
+package com.u002.mantis.proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+
+/**
+ * @author			amber
+ * @date 			2017-08-09 09:17:50
+ * @description 	默认代理实现JDK
+ */
+public class InternalProxy implements IProxy{
+
+	/**
+	 *
+	 * @param interfaces 类
+	 * @param invocationHandler 处理器
+	 * @param classLoader 类加载器
+	 * @return 代理对象
+	 * @param <T> 类型参数
+	 */
+	@Override
+	public <T> T createInstance(Class<T> interfaces, InvocationHandler invocationHandler, ClassLoader classLoader) {
+		return (T)Proxy.newProxyInstance(classLoader,new Class<?>[]{interfaces} , invocationHandler);
+	}
+	
+}
